05642621c703ebe24bc8158c939ab7cd48ed1bb2
Ricki Hirner <hirner@bitfire.at>
2026-03-23 22:21:29 +0100

    Use gradle version catalog (#5)
    * Move authentication to environment
    
    * Remove foojay from libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.2.0"
-    kotlin("plugin.serialization") version "2.2.0"
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
     application
 }
 
@@ -14,18 +14,14 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.bitfireAT:dav4jvm:59ab44eb6e")
-    implementation("io.ktor:ktor-client-auth:3.4.1")
-    implementation("io.ktor:ktor-client-cio:3.4.1")
-    implementation("io.ktor:ktor-client-logging:3.4.1")
-    implementation("io.ktor:ktor-server-cio:3.4.1")
-    implementation("io.ktor:ktor-server-content-negotiation:3.4.1")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:3.4.1")
-    implementation("io.modelcontextprotocol:kotlin-sdk:0.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
-    implementation("org.mnode.ical4j:ical4j:4.2.3")
-    implementation("com.google.dagger:dagger:2.56.2")
-    ksp("com.google.dagger:dagger-compiler:2.56.2")
+    implementation(libs.dav4jvm)
+    implementation(libs.bundles.ktor.client)
+    implementation(libs.bundles.ktor.server)
+    implementation(libs.mcp.kotlin.sdk)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.ical4j)
+    implementation(libs.dagger)
+    ksp(libs.dagger.compiler)
     testImplementation(kotlin("test"))
 }
 
