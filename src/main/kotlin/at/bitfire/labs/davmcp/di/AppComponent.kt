f742041d595c7694008d86f9ed607d8314c6d7fc
Ricki Hirner <hirner@bitfire.at>
2026-03-23 22:38:33 +0100

    Refactor McpServer and introduce dependency injection (#6)
    * Add McpTool interface
    
    * Add McpServer class and refactor main entry point
    
    - Extract server setup and configuration into new `McpServer` class
    - Simplify `Main.kt` by delegating server initialization and startup to `McpServer`
    
    * Refactor McpServer to use dependency injection
    
    - Add `@Inject` constructor to `McpServer`
    - Update `AppComponent` to provide `McpServer`
    - Simplify `Main.kt` by using injected `McpServer`
    
    * Add ToolsModule for dependency injection and refactor McpServer to support multiple tools
    
    - Introduce `ToolsModule` with Dagger setup for `AddEventTool`
    - Update `McpServer` to accept a set of `McpTool` instances
    - Modify `AppComponent` to include `ToolsModule` and remove direct `AddEventTool` exposure
    
    * Remove unused imports and clean up dependencies
    
    - Remove unused `JvmSuppressWildcards` import in `McpServer`
    - Remove unused `AddEventTool` import in `AppComponent`

--- src/main/kotlin/at/bitfire/labs/davmcp/di/AppComponent.kt
+++ src/main/kotlin/at/bitfire/labs/davmcp/di/AppComponent.kt
@@ -1,13 +1,13 @@
 package at.bitfire.labs.davmcp.di
 
-import at.bitfire.labs.davmcp.tools.AddEventTool
+import at.bitfire.labs.davmcp.McpServer
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class])
+@Component(modules = [AppModule::class, ToolsModule::class])
 interface AppComponent {
 
-    fun addEventTool(): AddEventTool
+    fun mcpServer(): McpServer
 
 }
